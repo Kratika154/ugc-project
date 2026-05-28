353458baefdb85295137fcde0afb424e69d03e98
Kratika Gupta <kratikagupta406@gmail.com>
2026-05-29 04:22:17 +0530

    Fixed footer text

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -46,7 +46,7 @@ export default function Footer() {
                 </div>
 
                 <p className="py-4 text-center text-sm text-gray-400">
-                    Build with ❤️ by Kratika & Kritik
+                 © {new Date().getFullYear()} Build with ❤️ by Kratika & Kritik
                 </p>
             </div>
         </motion.footer>
